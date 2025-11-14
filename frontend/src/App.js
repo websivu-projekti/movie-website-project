fadb3eb19983fec1494c10610250a5eb3b6f95cd
JeremiasGitit <jeremias.katajisto@gmail.com>
2025-11-14 13:37:22 +0200

    mergattu login ja signup. aloitettu editprofile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home" 
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/pages/EditProfile.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react"
+
+function EditProfile() {
+  const [profile, setProfile] = useState({ name: "", email: "" })
+  //const [loading, setLoading] = useState(true)
+
+  return (
+    
+  )
+}

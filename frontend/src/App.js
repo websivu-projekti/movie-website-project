0715b740d34b67ef962c1df906b8dc8e277f87a6
JeremiasGitit <jeremias.katajisto@gmail.com>
2025-11-15 22:30:51 +0200

    EditProfile raamit lisätty

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home" 
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
+import EditProfile from "./pages/EditProfile"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
       </Routes>
     </Router>
   )

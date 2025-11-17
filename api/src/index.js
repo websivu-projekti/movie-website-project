b00cfcbb5bb070da0adb4f84c48ea2ef6f9abb7a
Nella <nellasoudunsaari@gmail.com>
2025-11-17 13:00:37 +0200

    Backend toimii nyt jotenkin

--- api/src/index.js
+++ api/src/index.js
@@ -2,7 +2,6 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 
-import bookRouter from "./routers/book_router.js"
 import movieRouter from "./routers/movie_router.js" // uusi reitti
 
 const app = express()
@@ -17,38 +16,9 @@ app.get("/", async (req, res) => {
   res.send("Postgres API + TMDB esimerkki")
 })
 
-// olemassa olevat kirja-reitit
-app.use("/book", bookRouter)
-
 // uusi TMDB-elokuvat reitti
 app.use("/movies", movieRouter)
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
 })
-
-
-
-/*import express from "express";
-import cors from "cors";
-import "dotenv/config";
-
-import movieRouter from "./routers/movie_router.js";
-
-
-const app = express();
-const port = process.env.PORT;
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", async (req, res) => {
-  res.send("Postgres API esimerkki");
-});
-
-app.use("/movie", movieRouter);
-
-app.listen(port, () => {
-  console.log(`Server is listening port ${port}`);
-});*/
--- api/src/routers/movie_router.js
+++ api/src/routers/movie_router.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getMovies, getMovie, addMovie, updateMovie, deleteMovie } from "../controllers/movie_controller.js";
+import { getMovies, getMovie, addMovie, updateMovie, deleteMovie, getNowPlaying } from "../controllers/movie_controller.js";
 
 const movieRouter = Router();
 
 movieRouter.get("/", getMovies);
+movieRouter.get("/now-playing", getNowPlaying);
 movieRouter.get("/:id", getMovie);
 movieRouter.post("/", addMovie);
 movieRouter.put("/:id", updateMovie);

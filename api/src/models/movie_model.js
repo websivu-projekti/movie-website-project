35d46d739e3bffc35c7f2800fd27e8499dc1ab76
Nella <nellasoudunsaari@gmail.com>
2025-11-24 09:59:01 +0200

    Sprint 3 päivitetty branchiin, turhat moviemodel funktiot poistettu

--- api/src/models/movie_model.js
+++ api/src/models/movie_model.js
@@ -10,19 +10,7 @@ export async function getOne(id) {
   return result.rows.length > 0 ? result.rows[0] : null;
 }
 
-export async function addOne(book) {
+export async function addOne(content) {
   const result = await pool.query("INSERT INTO content (content_id, title,release_year,genre,description, poster_url, content_type) VALUES($1,$2,$3,$4,$5,$6,$7)", [content.content_id, content.title,content.release_year,content.genre,content.description,content.poster_url,content.content_type]);
-  return result.rows;
-}
-
-export async function updateOne(id,book) {
-  console.log("update:"+id);
-  const result = await pool.query("UPDATE book SET name=$1, author=$2, isbn=$3", [book.name, book.author,book.isbn]);
-  return result.rows;
-}
-
-export async function deleteOne(id) {
-  console.log("delete:"+id);
-  const result = await pool.query("DELETE FROM book WHERE id = $1", [id]);
   return result.rows;
 }

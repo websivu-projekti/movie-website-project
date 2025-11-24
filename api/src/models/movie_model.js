c0f6f89477c5440ffa3b9be985d27935905170a0
OliverK03 <oliver.kauppii@gmail.com>
2025-11-24 11:50:59 +0200

    model fix

--- api/src/models/movie_model.js
+++ api/src/models/movie_model.js
@@ -10,7 +10,7 @@ export async function getOne(id) {
   return result.rows.length > 0 ? result.rows[0] : null;
 }
 
-export async function addOne(book) {
+export async function addOne(content) {
   const result = await pool.query("INSERT INTO content (content_id, title,release_year,genre,description, poster_url, content_type) VALUES($1,$2,$3,$4,$5,$6,$7)", [content.content_id, content.title,content.release_year,content.genre,content.description,content.poster_url,content.content_type]);
   return result.rows;
 }

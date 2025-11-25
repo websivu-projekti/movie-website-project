4a32ea96ccb292e44ceaceb57f9ebda339c584db
Nella <nellasoudunsaari@gmail.com>
2025-11-25 17:09:32 +0200

    Melkein toimiva, melkein responsiivinen versio karusellista

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,5 +31,4 @@ function App() {
     </Router>
   )
 }
-import 'bootstrap/dist/css/bootstrap.min.css'
 export default App
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react"
 import "../index.css"
 import Header from '../components/header.jsx'
-import Carousel from 'react-bootstrap/Carousel'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 function Home() {
   const [movies, setMovies] = useState([])
@@ -31,9 +34,40 @@ function Home() {
       {loading
         ? <p>Ladataan elokuvia...</p>
         : <div className="movieRow">
-            {movies.map((movie, index) => (
-              <div key={index} class="movieCard">{movie.title}</div>
-            ))}
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={10}
+              loop={true}
+              watchSlidesProgress={true}
+              observeParents={true}
+              resizeObserver={true}
+              setWrapperSize={true}
+              navigation
+              breakpoints={{
+                740: {
+                  slidesPerView: 2,
+                  spaceBetween: 20
+                },
+                768: {
+                  slidesPerView: 3,
+                  spaceBetween: 30
+                },
+                1024:{
+                  slidesPerView: 4,
+                  spaceBetween: 50
+                }
+              }}
+              modules={[Navigation]}
+            >
+              {movies.map((movie, index) => (
+                <SwiperSlide key={index}>
+                  <div key={movie.id} class="movieCard">
+                    <img src={`http://image.tmdb.org/t/p/w185/${movie.poster_path}`}/>
+                    <p className="movieTitle"><a href="">{movie.title}</a></p>
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
           </div>
       }
     </div>

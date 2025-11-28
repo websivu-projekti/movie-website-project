3991cae760c25c4f75389dcb650bbb531f28d5b4
Nella <nellasoudunsaari@gmail.com>
2025-11-28 15:15:58 +0200

    Films-sivu nyt responsiivinen

--- frontend/src/pages/Films.js
+++ frontend/src/pages/Films.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import "../index.css"
 import "../filters.css"
 import Header from '../components/header.jsx'
-import Genres from '../components/genres.jsx'
-import FilterRating from "../components/filterrating.jsx"
-import SortBy from "../components/sortby.jsx"
-import FilterLanguages from "../components/languages.jsx"
-import FilterYear from "../components/filteryear.jsx"
-import FilterProviders from "../components/filterprovider.jsx"
-import FilterContent from "../components/filtercontent.jsx"
+import FiltersMenu from "../components/filtersmenu.jsx"
+import menuLogo from "../assets/menuicon.svg"
+import closeMenu from "../assets/closemenu.svg"
 
 function Films(){
   const [movies, setMovies] = useState([])
-  const [genres, setGenres] = useState([])
   const [loading, setLoading] = useState(true)
+  const mobileMenu = useRef(null)
 
   useEffect(() => {
     async function fetchMovies() {
@@ -32,40 +28,37 @@ function Films(){
     fetchMovies()
   }, [])
 
+  const openMobileMenu = () => {
+    mobileMenu.current.style.transform = 'translate3d(0vw, 0, 0)'
+  }
+
+  const closeMobileMenu = () => {
+    mobileMenu.current.style.transform = 'translate3d(-100vw, 0, 0)'
+  }
+
   return (
     <div className="container">
       <Header/>
       <h2>Films</h2>
       <div className="searchContainer">
+        <div className="filtersMenu">
+          <button className="filtermenuBtn" onClick={openMobileMenu}>
+            <img src={menuLogo}/>
+          </button>
+          <div className="mobileMenu" ref={mobileMenu}>
+            <button className="filtermenuBtn" onClick={closeMobileMenu}>
+              <img src={closeMenu}/>
+            </button>
+            <FiltersMenu/>
+          </div>
+        </div>
         <div className="filtersRow">
-          Filters
-          <a className="resetLink" href="">Reset filters</a>
-          {/* SORT BY */}
-          <div className="filterTitle">Sort by:</div>
-          <SortBy/>
-          {/* GENRES */}
-          <div className="filterTitle">Genres:</div>
-          <Genres/>
-          {/* LANGUAGES */}
-          <div className="filterTitle">Language:</div>
-          <FilterLanguages/>
-          {/* RATING */}
-          <div className="filterTitle">Rating:</div>
-          <FilterRating/>
-          {/* YEAR */}
-          <div className="filterTitle">Year:</div>
-          <FilterYear/>
-          {/* PROVIDERS */}
-          <div className="filterTitle">Providers:</div>
-          <FilterProviders/>
-          {/* CONTENT */}
-          <div className="filterTitle">Content:</div>
-          <FilterContent/>
+          <FiltersMenu/>
         </div>
         <div className="movieRow">
           {movies.map((movie, index) => (
             <div key={index} class="movieCard">
-                <img src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`}/>
+                <img className="moviePoster" src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`}/>
                 <p className="movieTitle"><a className="movieLink" href="">{movie.title}</a></p>
             </div>
           ))}

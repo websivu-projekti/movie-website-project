e1d0f5ab77caef6faef0047277c2470f3e3eaf94
Jebachu <jenkki.seurujarvi@hotmail.com>
2025-11-29 21:23:55 +0200

    Tallennetaan kaikki nykyiset tiedostot jenna branchiin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
+import Home from "./pages/Home" 
 import Cinema from "./pages/Cinema"
 import Profile from "./pages/Profile"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import EditProfile from "./pages/EditProfile"
 import MovieList from "./pages/MovieList"
-import Groups from "./pages/Groups"
+import MyGroups from "./pages/MyGroups"
 import Films from "./pages/Films"
-
+import GroupsList from "./pages/GroupsList"
+import GroupDetail from "./pages/GroupDetail"
 
 function App() {
   return (
@@ -21,14 +22,14 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/cinema" element={<Cinema />} />
-        <Route path="/films" element={<Films />} />
-        <Route path="/profile" element={<Profile />} />
         <Route path="/editprofile" element={<EditProfile />} />
-        <Route path="/movielist" element={<MovieList />} />
-        <Route path="/groups" element={<Groups />} />
+        <Route path="/mylists" element={<MovieList />} />
+        <Route path="/mygroups" element={<MyGroups />} />
+        <Route path="/groups" element={<GroupsList />} />
+        <Route path="/groups/:groupId" element={<GroupDetail />} />
       </Routes>
     </Router>
   )
 }
+
 export default App

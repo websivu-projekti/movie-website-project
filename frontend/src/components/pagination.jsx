1babfcaba3c2e0d59f65fdb28e28ed29a4448171
Nella <nellasoudunsaari@gmail.com>
2025-12-03 18:12:52 +0200

    paginatio ei mene enää <1

--- frontend/src/components/pagination.jsx
+++ frontend/src/components/pagination.jsx
@@ -1,24 +1,40 @@
-import React, { useEffect, useState } from "react"
-
+import React, { useState } from "react"
 export default function Pagination({currentPage, setCurrentPage}){
+    const [ className, setClassName ] = useState('paginationBtnDisabled')
+    const [ disabled, setDisabled ] = useState(true)
+    const [ lastPage, setLastPage ] = useState(0)
+
     const nextPage = () => {
+        if(currentPage >= 1){
+            setClassName('paginationBtn')
+            setDisabled(false)
+        }
         setCurrentPage(currentPage => currentPage + 1)
+        setLastPage(lastPage => lastPage + 1 )
     }
 
     const prevPage = () => {
+        if(lastPage === 1){
+            setClassName('paginationBtnDisabled')
+        }
         setCurrentPage(currentPage => currentPage - 1)
+        setLastPage(lastPage => lastPage - 1)
     }
 
     return(
         <div className="pagination">
             <button 
-            className="paginationBtn"
-            onClick={prevPage}>
+            className={className}
+            onClick={prevPage}
+            disabled={lastPage === 0}
+            >
               Prev
             </button>
+            <div>Page {currentPage}</div>
             <button 
-            className="paginationBtn"
-            onClick={nextPage}>
+            className='paginationBtn'
+            onClick={nextPage}
+            >
               Next
             </button>
         </div> 

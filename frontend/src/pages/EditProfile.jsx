dc692d77609f0151c833d8d01a7dc3e43ed889bf
JeremiasGitit <jeremias.katajisto@gmail.com>
2025-12-17 12:55:46 +0200

    Profiilikuvan vaihto raamit

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -3,6 +3,17 @@ import React, { useState } from "react";
 import "../EditProfile.css";
 import Header from '../components/header.jsx'
 import { useAuth } from "../context/AuthContext.js";
+import pf1 from "../assets/icons/pf1.png"
+import pf2 from "../assets/icons/pf2.png"
+import pf3 from "../assets/icons/pf3.png"
+import pf4 from "../assets/icons/pf4.png"
+import pf5 from "../assets/icons/pf5.png"
+import pf6 from "../assets/icons/pf6.png"
+import pf7 from "../assets/icons/pf7.png"
+import pf8 from "../assets/icons/pf8.png"
+import pf9 from "../assets/icons/pf9.png"
+import closeMenu from "../assets/closemenu.svg"
+
 
 function EditProfile() {
   const { user } = useAuth()
@@ -11,6 +22,8 @@ function EditProfile() {
   const [newPasswordConfirm, setNewPasswordConfirm] = useState("");
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
+  const [showChangePfp, setShowChangePfp] = useState(false)
+  const [pfp_url, setNewPfp] = useState("pf1")
 
 
   const handleChangePassword = async (e) => {
@@ -25,10 +38,10 @@ function EditProfile() {
     }
 
     try {
-      
+
       const token = localStorage.getItem("token");
-      
-      
+
+
       const response = await fetch("http://localhost:3001/auth/editprofile", {
         method: "PATCH",
         headers: {
@@ -65,32 +78,63 @@ function EditProfile() {
   };
 
   const handleDeleteAccount = async () => {
-      if (window.confirm(`Haluatko varmasti poistaa käyttäjäsi, ${user.username}? Toimintoa ei voi perua.`)) {
-        try {
-          const response = await fetch("http://localhost:3001/auth/account", {
-            method: 'DELETE',
-            headers: {
-              'Content-Type' : 'application/json',
-              'Authorization' : `Bearer ${user.token}`
-            },
-            body: JSON.stringify({ username: user.username })
-          });
-  
-          const data = await response.json()
-  
-          if (!response.ok) {
-            throw new Error(data.error || "Failed to fetch profile")
-          }
-  
-          alert('Käyttäjä poistettu onnistuneesti.')
-          logout()
-          navigate('/')
-  
-        } catch (err) {
-          alert(`Error: ${error.message}`)
-          }
+    if (window.confirm(`Haluatko varmasti poistaa käyttäjäsi, ${user.username}? Toimintoa ei voi perua.`)) {
+      try {
+        const response = await fetch("http://localhost:3001/auth/account", {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${user.token}`
+          },
+          body: JSON.stringify({ username: user.username })
+        });
+
+        const data = await response.json()
+
+        if (!response.ok) {
+          throw new Error(data.error || "Failed to fetch profile")
         }
+
+        alert('Käyttäjä poistettu onnistuneesti.')
+        logout()
+        navigate('/')
+
+      } catch (err) {
+        alert(`Error: ${error.message}`)
+      }
     }
+  }
+  const openChangePfp = () => {
+    setShowChangePfp(!showChangePfp)
+  }
+  const closeChangePfp = () => {
+    setShowChangePfp(false)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3001/auth/editprofile/pfp", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${user.token}`,
+        },
+        body: JSON.stringify({ pfp_url }),
+      });
+      const data = await response.json()
+
+      if(response.ok){
+        //fetchPfp()
+        closeChangePfp()
+      }else{
+        throw new Error(data.error || "An error occurred while changing profile picture")
+      }
+    }catch(err){
+      console.log(err)
+    }
+  }
 
   return (
     <div className="container">
@@ -98,15 +142,15 @@ function EditProfile() {
       <div className="content">
         <h1 className="editTitle">Edit Profile</h1>
         <div className="imageContainer">
-          <img src="" alt="Profile picture" className="editPfp"></img>
+          <img src={pfp_url} alt="Profile picture" className="editPfp"></img>
         </div>
-        <button type="submit" className="changePfp button">
+        <button type="submit" className="changePfp button" onClick={openChangePfp}>
           Change Profile Picture
         </button>
         <form className="accessibility">
-          <h4>
+          <h3>
             Settings
-          </h4>
+          </h3>
           <div className="field">
             <input type="checkbox" />
             <label className="settingsLabel">Dyslexia friendly font</label>
@@ -154,6 +198,121 @@ function EditProfile() {
           </button>
         </form>
       </div>
+      {showChangePfp &&
+        <div className="pfpMenuContainer">
+          <div className="pfpMenu">
+            <h2 className="pfpMenuTitle">Change profile picture</h2>
+            <button className="closePfpMenu" onClick={closeChangePfp}>
+              <img src={closeMenu} />
+            </button>
+            <form className="choosePfpForm">
+              <label className="choosePfpLabel pfpIconLabel"> Choose profile picture: </label>
+              <div className="pfpIconGrid">
+                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf1"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf1} alt="Icon option 1, dog with green background" />
+                </label>
+                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf2"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf2} alt="Icon option 2, dog with yellow background" />
+                </label>
+                                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf3"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf3} alt="Icon option 3, bunny with blue background" />
+                </label>
+                                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf4"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf4} alt="Icon option 4, fish with violet background" />
+                </label>
+                                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf5"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf5} alt="Icon option 5, fish with pink background" />
+                </label>
+                                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf6"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf6} alt="Icon option 6, seagull with orange background" />
+                </label>
+                                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf7"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf7} alt="Icon option 7, cat with yellow background" />
+                </label>
+                                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf8"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf8} alt="Icon option 8, catfish with blue background" />
+                </label>
+                                <label className="pfpIconLabel">
+                  <input type="radio"
+                    className="pfpIconRadio"
+                    name="profilePicture"
+                    value="pf9"
+                    onChange={(e) => {
+                      setNewPfp(e.target.value)
+                    }}
+                  />
+                  <img className="createPfpIcon" src={pf9} alt="Icon option 9, sharkhorse with pink background" />
+                </label>
+              </div>
+              <button onClick={handleSubmit} className="choosePfpBtn">Set profile picture</button>
+            </form>
+          </div>
+        </div>
+      }
     </div>
   );
 }
